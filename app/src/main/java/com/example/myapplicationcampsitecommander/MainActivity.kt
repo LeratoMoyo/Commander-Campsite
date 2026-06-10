b25a25bac9ae5e8693bc39474f931157b3e3325d
Lerato Moyo <liramoyo46@gmail.com>
2026-06-10 12:46:22 +0200

    starting on main activity.
    separate splash screen created

--- app/src/main/java/com/example/myapplicationcampsitecommander/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationcampsitecommander/MainActivity.kt
@@ -1,20 +1,11 @@
 package com.example.myapplicationcampsitecommander
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.delay
 import androidx.appcompat.app.AppCompatActivity
 
-class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class MainActivity: AppCompatActivity(){
 
-        setContentView(R.layout.activity_splash)
-
-    }
 }
 
